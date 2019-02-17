d98e96b6064918fe5e19d3b6cc425699a3239221
Min <msuh1989@gmail.com>
2019-02-17 21:41:01 +0900

    react course restart. be a finisher

---
+++ react-complete-guide/Snippets.js
@@ -0,0 +1,5 @@
+
+//how react component is rendered
+//react parases the jsx code
+//react-dom allow us to render a javascript function as a component to the real DOM
+ReactDom.render(<Person/>, document.querySelector('#p1'))
